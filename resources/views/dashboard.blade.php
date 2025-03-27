b2ea759cdcea5cd33be76060179fd3163d18f264
ghobelmann <99198193+ghobelmann@users.noreply.github.com>
2025-03-27 10:28:25 -0500

    image

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
  <div class="text-center">
      <flux:heading size="xl">DeSoto Golf Coaches Page</flux:heading>
 
-     <img src="{{ asset('images/teams/85.png') }}" alt="Team Logo">
+     <img src="{{ asset('images/teams/85.png') }}" alt="Team Logo" width="250" height="250">
 
  </div>
 
